aa174117c48a237a847310cd7c34f2867e6986ee
Michael Luo <michael.luo@Michaels-MacBook-Pro.local>
2021-07-21 22:34:00 +1000

    add air

--- api/go.mod
+++ api/go.mod
@@ -4,5 +4,11 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.2
-	go.mongodb.org/mongo-driver v1.7.0
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/rs/xid v1.3.0
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- api/main.go
+++ api/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"net/http"
+	"time"
+
+	"microservice/src/models"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
 func IndexHandler(c *gin.Context) {
@@ -12,8 +16,34 @@ func IndexHandler(c *gin.Context) {
 	})
 }
 
+var recipes []models.Recipe
+
+func NewReciptHandler(c *gin.Context) {
+	var recipe models.Recipe
+
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+
+		c.JSON(http.StatusBadRequest, gin.H{
+
+			"error": err.Error()})
+
+		return
+
+	}
+
+	recipe.ID = xid.New().String()
+
+	recipe.PublishedAt = time.Now()
+
+	recipes = append(recipes, recipe)
+
+	c.JSON(http.StatusOK, recipe)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/", IndexHandler)
+	router.POST("/recipes", NewReciptHandler)
+
 	http.ListenAndServe(":8080", router)
 }
