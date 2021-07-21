5c90332e64dd2e29a2ff55eaf2ff0415755bffc5
Michael Luo <michael.luo@Michaels-MacBook-Pro.local>
2021-07-21 22:19:59 +1000

    add first server

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,5 @@
-module example.com/online-store
+module microservice
 
 go 1.16
+
+require github.com/gin-gonic/gin v1.7.2
--- api/main.go
+++ api/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	"log"
+	"net/http"
 
-	"example.com/online-store/src/routes/router"
+	"github.com/gin-gonic/gin"
 )
 
+func IndexHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "hello world",
+	})
+}
+
 func main() {
-	log.Println("Main log....")
-	router.RunAPI(":9090")
+	router := gin.Default()
+	router.GET("/", IndexHandler)
+	http.ListenAndServe(":8080", router)
 }
